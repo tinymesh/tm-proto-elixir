5f978d276164d6109292270047e6688e933c542c
Olav Frengstad <olav@fwt.no>
2014-09-09 23:01:47 +0200

    Bump tag

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Tinymesh.Proto.Mixfile do
 
   def project do
     [ app: :tinymesh,
-      version: "0.3.2",
+      version: "0.4.0-1",
       deps: [
         {:exprof, "~> 0.1.0", only: :test, env: :test}
       ]
