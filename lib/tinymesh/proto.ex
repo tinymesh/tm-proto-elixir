c82ee7d66f50c60b36c672580d9235dbdf2d6bf3
Olav Frengstad <olav@fwt.no>
2014-12-09 02:22:36 +0800

    Accept unknown NAK reasons, since they do appear in the protocol

--- lib/tinymesh/proto.ex
+++ lib/tinymesh/proto.ex
@@ -984,6 +984,7 @@ defmodule Tinymesh.Proto do
   defp detail_to_str(n), do:
     raise(PacketError, field: "detail", message: "invalid event detail '#{n}' (-> str)")
 
+  defp nak_trigger_to_str( 0), do: "unknown"
   defp nak_trigger_to_str( 1), do: "packet_length"
   defp nak_trigger_to_str( 2), do: "bad_gw_config"
   defp nak_trigger_to_str( 3), do: "bad_gw_packet_format"
@@ -995,6 +996,7 @@ defmodule Tinymesh.Proto do
   defp nak_trigger_to_str(n), do:
     raise(PacketError, field: "reason", message: "invalid nak reason '#{n}' (-> str)")
 
+  defp nak_trigger_to_str("unknown"),                do:  0
   defp nak_trigger_to_int("packet_length"),          do:  1
   defp nak_trigger_to_int("bad_gw_config"),          do:  2
   defp nak_trigger_to_int("bad_gw_packet_format"),   do:  3
