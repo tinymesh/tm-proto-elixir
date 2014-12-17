a9e19891ae9545ae1ebab899693833a6d4607358
Olav Frengstad <olav@fwt.no>
2014-12-17 22:21:56 +0100

    Add documentation
    Automtically store markdown files with the documentation for use by
    external sources

--- lib/tinymesh/config/packer.ex
+++ lib/tinymesh/config/packer.ex
@@ -1,122 +1,153 @@
 defmodule Tinymesh.Config.Packer do
-
   defmodule Error do
     defexception parameter: nil, addr: nil, message: ""
   end
 
-  Module.register_attribute __MODULE__, :config, persist: true
-  @config [
-    {"rf.channel",                  %{addr:  0, range: 1..83}},
-    {"rf.power",                    %{addr:  1, range: 1..5}},
-    {"rf.data_rate",                %{addr:  2, range: 1..6}},
-    {"net.rssi_threshold",          %{addr:  4, range: 160..210}},
-    {"net.rssi_cc_assesment",       %{addr:  5, range: 100..210}},
-    {"net.hiam_time",               %{addr:  6, range: 1..10}},
-    {"ima.time",                    %{addr:  7, range: 1..255}},
-    {"ima.time_base",               %{addr: 85,  range: 1..255}},
-    {"ima.on_connect",              %{addr: 94,  enum: [0,1]}},
-    {"ima.data_field",              %{addr: 106, enum: [0,1,2], since: "1.40"}},
-    {"ima.address_field",           %{addr: 107, enum: [0,1,2,3], since: "1.40"}},
-    {"ima.trig_hold",               %{addr: 108, range: 0..255,   since: "1.40"}},
+  Module.register_attribute __MODULE__, :config, accumulate: true
 
-    {"net.connect_check_time",      %{addr:  8, range: 4..20}},
-    {"net.max_jump_level",          %{addr:  9, range: 1..255}},
-    {"net.max_jump_count",          %{addr: 10, range: 1..255}},
-    {"net.max_packet_latency",      %{addr: 11, range: 1..255}},
+  @protovsn "1.41"
 
-    {"net.tx_retry_limit",          %{addr: 12, range: 1..255}},
+  @config {"rf.channel",                  %{addr:  0, range: 1..83}}
+  @config {"rf.power",                    %{addr:  1, range: 1..5}}
+  @config {"rf.data_rate",                %{addr:  2, range: 1..6}}
+  @config {"net.rssi_threshold",          %{addr:  4, range: 160..210}}
+  @config {"net.rssi_cc_assesment",       %{addr:  5, range: 100..210}}
+  @config {"net.hiam_time",               %{addr:  6, range: 1..10}}
+  @config {"ima.time",                    %{addr:  7, range: 1..255}}
+  @config {"ima.time_base",               %{addr: 85,  range: 1..255}}
+  @config {"ima.on_connect",              %{addr: 94,  enum: [0,1]}}
+  @config {"ima.data_field",              %{addr: 106, enum: [0,1,2], since: "1.40"}}
+  @config {"ima.address_field",           %{addr: 107, enum: [0,1,2,3], since: "1.40"}}
+  @config {"ima.trig_hold",               %{addr: 108, range: 0..255,   since: "1.40"}}
 
-    {"uart.timeout",                %{addr: 13, range: 1..255}},
+  @config {"net.connect_check_time",      %{addr:  8, range: 4..20}}
+  @config {"net.max_jump_level",          %{addr:  9, range: 1..255}}
+  @config {"net.max_jump_count",          %{addr: 10, range: 1..255}}
+  @config {"net.max_packet_latency",      %{addr: 11, range: 1..255}}
 
-    {"device.protocol_mode",        %{addr:  3, enum: [0, 1]}},
-    {"device.type",                 %{addr: 14, enum: [1,2,3], multi: true, since:  "1.40"}},
-    {"device.type",                 %{addr: 14, enum: [1,2],   before: "1.40"}},
-    {"device.locator",              %{addr: 44, enum: [0,1]}},
-    {"device.uid",                  %{addr: 45, size: 4, range: 0..4294967295, endian: :little}},
-    {"device.sid",                  %{addr: 49, size: 4, range: 0..4294967295, endian: :little}},
+  @config {"net.tx_retry_limit",          %{addr: 12, range: 1..255}}
 
-    {"net.excellent_rssi",          %{addr: 15, range: 0..255, since: "1.37"}},
+  @config {"uart.timeout",                %{addr: 13, range: 1..255}}
 
-    {"gpio_0.config",               %{addr: 16, enum:  [0,1,2,4]}},
-    {"gpio_0.trigger",              %{addr: 24, enum:  [0,1,2,3]}},
-    {"gpio_0.analogue_high_trig",   %{addr: 33, range: 0..2047, size: 2}},
-    {"gpio_0.analogue_low_trig",    %{addr: 35, range: 0..2047, size: 2}},
-    {"gpio_0.analogue_sample_rate", %{addr: 37, range: 0..255}},
-    {"gpio_1.config",               %{addr: 17, enum:  [0,1,2,4]}},
-    {"gpio_1.trigger",              %{addr: 25, enum:  [0,1,2,3]}},
-    {"gpio_1.analogue_high_trig",   %{addr: 38, range: 0..2047, size: 2}},
-    {"gpio_1.analogue_low_trig",    %{addr: 40, range: 0..2047, size: 2}},
-    {"gpio_1.analogue_sample_rate", %{addr: 42, range: 0..255}},
-    {"gpio_2.config",               %{addr: 18, enum:  [0,1,4]}},
-    {"gpio_2.trigger",              %{addr: 26, enum:  [0,1,2,3]}},
-    {"gpio_3.config",               %{addr: 19, enum:  [0,1,4]}},
-    {"gpio_3.trigger",              %{addr: 27, enum:  [0,1,2,3]}},
-    {"gpio_4.config",               %{addr: 20, enum:  [0,1,4]}},
-    {"gpio_4.trigger",              %{addr: 28, enum:  [0,1,2,3]}},
-    {"gpio_5.config",               %{addr: 21, enum:  [0,1,4]}},
-    {"gpio_5.trigger",              %{addr: 29, enum:  [0,1,2,3]}},
-    {"gpio_6.config",               %{addr: 22, enum:  [0,1,4]}},
-    {"gpio_6.trigger",              %{addr: 30, enum:  [0,1,2,3]}},
-    {"gpio_7.config",               %{addr: 23, enum:  [0,1,3,4]}},
-    {"gpio_7.trigger",              %{addr: 31, enum:  [0,1,2,3]}},
-    {"gpio_7.pwm_default",          %{addr: 95, range: 0..100}},
+  @config {"device.protocol_mode",        %{addr:  3, enum: [0, 1]}}
+  @config {"device.type",                 %{addr: 14, enum: [1,2,3], multi: true, since:  "1.40"}}
+  @config {"device.type",                 %{addr: 14, enum: [1,2],   before: "1.40"}}
+  @config {"device.locator",              %{addr: 44, enum: [0,1]}}
+  @config {"device.uid",                  %{addr: 45, size: 4, range: 0..4294967295, endian: :little}}
+  @config {"device.sid",                  %{addr: 49, size: 4, range: 0..4294967295, endian: :little}}
 
-    {"gpio.input_debounce",         %{addr: 32, range: 0..255}},
-    {"uart.cts_hold_time",          %{addr: 43, range: 1..255}},
+  @config {"net.excellent_rssi",          %{addr: 15, range: 0..255, since: "1.37"}}
 
-    {"uart.baud_rate",              %{addr: 53, enum: [1,2,3,4,5,6,7,8,9,10,11]}},
-    {"uart.bits",                   %{addr: 54, enum: [8,9],   since: "1.21"}},
-    {"uart.parity",                 %{addr: 55, enum: [0,1],   since: "1.21"}},
-    {"uart.stop_bits",              %{addr: 56, enum: [1,2],   since: "1.21"}},
-    {"uart.flow_control",           %{addr: 58, range: 1..59,  since: "1.31"}},
-    {"uart.buffer_margin",          %{addr: 59, range: 0..100, since: "1.31"}},
+  @config {"gpio_0.config",               %{addr: 16, enum:  [0,1,2,4]}}
+  @config {"gpio_0.trigger",              %{addr: 24, enum:  [0,1,2,3]}}
+  @config {"gpio_0.analogue_high_trig",   %{addr: 33, range: 0..2047, size: 2}}
+  @config {"gpio_0.analogue_low_trig",    %{addr: 35, range: 0..2047, size: 2}}
+  @config {"gpio_0.analogue_sample_rate", %{addr: 37, range: 0..255}}
+  @config {"gpio_1.config",               %{addr: 17, enum:  [0,1,2,4]}}
+  @config {"gpio_1.trigger",              %{addr: 25, enum:  [0,1,2,3]}}
+  @config {"gpio_1.analogue_high_trig",   %{addr: 38, range: 0..2047, size: 2}}
+  @config {"gpio_1.analogue_low_trig",    %{addr: 40, range: 0..2047, size: 2}}
+  @config {"gpio_1.analogue_sample_rate", %{addr: 42, range: 0..255}}
+  @config {"gpio_2.config",               %{addr: 18, enum:  [0,1,4]}}
+  @config {"gpio_2.trigger",              %{addr: 26, enum:  [0,1,2,3]}}
+  @config {"gpio_3.config",               %{addr: 19, enum:  [0,1,4]}}
+  @config {"gpio_3.trigger",              %{addr: 27, enum:  [0,1,2,3]}}
+  @config {"gpio_4.config",               %{addr: 20, enum:  [0,1,4]}}
+  @config {"gpio_4.trigger",              %{addr: 28, enum:  [0,1,2,3]}}
+  @config {"gpio_5.config",               %{addr: 21, enum:  [0,1,4]}}
+  @config {"gpio_5.trigger",              %{addr: 29, enum:  [0,1,2,3]}}
+  @config {"gpio_6.config",               %{addr: 22, enum:  [0,1,4]}}
+  @config {"gpio_6.trigger",              %{addr: 30, enum:  [0,1,2,3]}}
+  @config {"gpio_7.config",               %{addr: 23, enum:  [0,1,3,4]}}
+  @config {"gpio_7.trigger",              %{addr: 31, enum:  [0,1,2,3]}}
+  @config {"gpio_7.pwm_default",          %{addr: 95, range: 0..100}}
 
-    {"device.part",                 %{addr: 60, def?: false, size: 12, type: :binary, delimiter: [nil, ","],     ro: true}},
-    {"device.hw_revision",          %{addr: 70, def?: false, size: 5,  type: :vsn,    delimiter: [",", ","],     ro: true}},
-    {"device.fw_revision",          %{addr: 75, def?: false, size: 5,  type: :vsn,    delimiter: [",", <<255>>], ro: true}},
+  @config {"gpio.input_debounce",         %{addr: 32, range: 0..255}}
+  @config {"uart.cts_hold_time",          %{addr: 43, range: 1..255}}
 
-    {"security.level",              %{addr: 81, enum: [0,1,2], since: "1.31"}},
+  @config {"uart.baud_rate",              %{addr: 53, enum: [1,2,3,4,5,6,7,8,9,10,11]}}
+  @config {"uart.bits",                   %{addr: 54, enum: [8,9],   since: "1.21"}}
+  @config {"uart.parity",                 %{addr: 55, enum: [0,1],   since: "1.21"}}
+  @config {"uart.stop_bits",              %{addr: 56, enum: [1,2],   since: "1.21"}}
+  @config {"uart.flow_control",           %{addr: 58, range: 1..59,  since: "1.31"}}
+  @config {"uart.buffer_margin",          %{addr: 59, range: 0..100, since: "1.31"}}
 
-    # ima under ima.step
-    {"end_device.keepalive",        %{addr: 86,  range: 0..255,                since: "1.40"}},
-    {"end_device.wakeon",           %{addr: 87,  range: 0..15,                 since: "1.40"}},
-    {"end_device.wakeon_port",      %{addr: 109, enum:  [0,1,2,3,4,5,6,7,255], since: "1.40"}},
+  @config {"device.part",                 %{addr: 60, def?: false, size: 12, type: :binary, delimiter: [nil, ","],     ro: true}}
+  @config {"device.hw_revision",          %{addr: 70, def?: false, size: 5,  type: :vsn,    delimiter: [",", ","],     ro: true}}
+  @config {"device.fw_revision",          %{addr: 75, def?: false, size: 5,  type: :vsn,    delimiter: [",", <<255>>], ro: true}}
 
-    {"device.indicators_timeout",   %{addr: 89, range: 0..255, since: "1.40"}},
+  @config {"security.level",              %{addr: 81, enum: [0,1,2], since: "1.31"}}
 
-    {"device.sniff_neighbours",     %{addr: 90, enum: [0, 1],  since: "1.40"}},
-    {"device.command_ack",          %{addr: 91, enum: [0, 1], since: "1.20"}},
+  # ima under ima.ste
+  @config {"end_device.keepalive",        %{addr: 86,  range: 0..255,                since: "1.40"}}
+  @config {"end_device.wakeon",           %{addr: 87,  range: 0..15,                 since: "1.40"}}
+  @config {"end_device.wakeon_port",      %{addr: 109, enum:  [0,1,2,3,4,5,6,7,255], since: "1.40"}}
 
-    {"device.sleep_or_rts_pin",     %{addr: 93, enum: [0, 1], since: "1.40"}},
+  @config {"device.indicators_timeout",   %{addr: 89, range: 0..255, since: "1.40"}}
 
-    # pwm_default located in gpio_7.pwm_default
+  @config {"device.sniff_neighbours",     %{addr: 90, enum: [0, 1],  since: "1.40"}}
+  @config {"device.command_ack",          %{addr: 91, enum: [0, 1], since: "1.20"}}
 
-    {"pulse_counter.mode",          %{addr: 96, enum: [0,1,3], since: "1.40"}},
-    {"pulse_counter.debounce",      %{addr: 97, range: 0..255, since: "1.40"}},
+  @config {"device.sleep_or_rts_pin",     %{addr: 93, enum: [0, 1], since: "1.40"}}
 
-    {"net.connect_change_margin",   %{addr: 98, range: 0..255, since: "1.37"}},
+  # pwm_default located in gpio_7.pwm_default
 
-    {"cluster.device_limit",        %{addr:  99, range: 5..100, since: "1.34"}},
-    {"cluster.rssi_threshold",      %{addr: 100, range: 40..100, since: "1.34"}},
+  @config {"pulse_counter.mode",          %{addr: 96, enum: [0,1,3], since: "1.40"}}
+  @config {"pulse_counter.debounce",      %{addr: 97, range: 0..255, since: "1.40"}}
 
-    {"device.detect_busy_network",  %{addr: 101, enum: [0,1,2], since: "1.20"}},
+  @config {"net.connect_change_margin",   %{addr: 98, range: 0..255, since: "1.37"}}
 
-    {"rf_jamming.detect",           %{addr: 102, range: 0..100,               since: "1.34"}},
-    {"rf_jamming.port",             %{addr: 103, enum: [0,1,2,3,4,5,6,7,255], since: "1.34"}},
+  @config {"cluster.device_limit",        %{addr:  99, range: 5..100, since: "1.34"}}
+  @config {"cluster.rssi_threshold",      %{addr: 100, range: 40..100, since: "1.34"}}
 
-    {"pulse_counter.feedback_port", %{addr: 104, enum: [0,1,2,3,4,5,6,7,255], since: "1.40"}},
-    {"pulse_counter.feedback",      %{addr: 105, enum: [0,2],                 since: "1.40"}},
+  @config {"device.detect_busy_network",  %{addr: 101, enum: [0,1,2], since: "1.20"}}
 
-    # ima under ima.*
-    # end device awake port under end_device.wakeon_port
+  @config {"rf_jamming.detect",           %{addr: 102, range: 0..100,               since: "1.34"}}
+  @config {"rf_jamming.port",             %{addr: 103, enum: [0,1,2,3,4,5,6,7,255], since: "1.34"}}
 
-    {"group.membership",            %{addr: 113, set: [], size: 8, since:  "1.40"}},
-    {"net.command_accept_time",     %{addr: 121, range: 0..255}},
-    {"net.command_retries",         %{addr: 122, range: 0..127}},
+  @config {"pulse_counter.feedback_port", %{addr: 104, enum: [0,1,2,3,4,5,6,7,255], since: "1.40"}}
+  @config {"pulse_counter.feedback",      %{addr: 105, enum: [0,2],                 since: "1.40"}}
 
-    {"rf.mac_rnd_mask_1",           %{addr: 123, enum: [116,63,31,15,7,3], since: "1.38"}},
-    {"rf.mac_rnd_mask_2",           %{addr: 124, enum: [116,63,31,15,7,3], since: "1.38"}}
-  ]
+  @config # ima under ima.*
+  @config # end device awake port under end_device.wakeon_port
+
+  @config {"group.membership",            %{addr: 113, set: [], size: 8, since:  "1.40"}}
+  @config {"net.command_accept_time",     %{addr: 121, range: 0..255}}
+  @config {"net.command_retries",         %{addr: 122, range: 0..127}}
+
+  @config {"rf.mac_rnd_mask_1",           %{addr: 123, enum: [116,63,31,15,7,3], since: "1.38"}}
+  @config {"rf.mac_rnd_mask_2",           %{addr: 124, enum: [116,63,31,15,7,3], since: "1.38"}}
+
+  # Save documentation on compile so it can be picked up by external tools
+  @after_compile __MODULE__
+  def __after_compile__(env, _bytecode) do
+    config = Module.get_attribute __MODULE__, :config
+
+    doc = ["""
+    ## Configuration Options
+    {: data-path=proto.configuration }
+
+    **Note:** This documentation was generated for version
+    #{@protovsn}". If your version of the firmware is newer, look for
+    the updated version of this documentation at the bottom of this page.
+
+    ## Parameters
+    """]
+
+    since = fn
+      (nil) -> ""
+      (vsn) -> "(since: #{vsn})"
+    end
+    buf = doc ++ Enum.map(config, fn
+      ({field, e = %{type: :vsn}})      -> "  * `#{field}` - string: <major.minor> #{since.(e[:since])}"
+      ({field, e = %{type: :binary}})   -> "  * `#{field}` - string #{since.(e[:since])}"
+      ({field, e = %{enum: vals}})      -> "  * `#{field}` - enum: #{Enum.join(vals, ", ")} #{since.(e[:since])}"
+      ({field, e = %{set: _, size: n}}) -> "  * `#{field}` - set (max size: #{n} #{since.(e[:since])}"
+      ({field, e = %{range: range}})    -> "  * `#{field}` - integer: #{inspect range} #{since.(e[:since])}"
+    end) |> Enum.join "\n"
+
+    :ok = File.write "./doc/Configuration.md", buf
+  end
 
   @doc """
   Unpacks a configuration blob into a map
