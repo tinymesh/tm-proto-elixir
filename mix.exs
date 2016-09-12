aa90eff4d7454d33a96888afb95f9b2c8d70f6cc
Olav Frengstad <olav@fwt.no>
2016-09-12 17:18:04 +0200

    Add exprof

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule Tinymesh.Proto.Mixfile do
     [ app: :tinymesh,
       version: "0.4.0-2",
       deps: [
-        {:poison, "~> 1.2", only: :dev, env: :test}
+        {:poison, "~> 1.2", only: :dev, env: :test},
+#        {:exprof, "~> 0.1", only: :dev, env: :test}
       ]
     ]
   end
