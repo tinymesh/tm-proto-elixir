31233f52f72766cab2697597bcf5d7b7bbb20f5c
Olav Frengstad <olav@fwt.no>
2017-04-23 20:37:13 +0200

    Vsn 0.4.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Tinymesh.Proto.Mixfile do
 
   def project do
     [ app: :tinymesh,
-      version: "0.4.0-2",
+      version: "0.4.1",
       deps: [
         {:poison, "~> 1.2", only: :dev, env: :test},
 #        {:exprof, "~> 0.1", only: :dev, env: :test}
